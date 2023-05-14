2b9f2d6c6f8d853892d25cc446e8efe4ee6bdde8
PavanRockz9 <pavanchamp69@gmail.com>
2023-05-14 19:32:08 +0530

    Updated Hello, World! program to palindrome strings

--- program.py
+++ program.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+a='mom'
+
+b='sis'
+
+c='bob'
+
+print(a==a[::-1]) # 'mom' is a palindrome - True
+
+print(b==b[::-1]) # 'sis' is a palindrome - True
+
+print(c==c[::-1]) # 'bob' is a palindrome - True
